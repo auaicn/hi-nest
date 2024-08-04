67c12d4baf11996989295560a622375dc8f4cd64
Kyung Ho, Park <crorowslowpp@gmail.com>
2024-08-05 00:53:13 +0900

    feat: movies controller

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,88 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  movies: {
+    id: number;
+    name: string;
+  }[] = [
+    {
+      id: 1,
+      name: 'one',
+    },
+  ];
+
+  @Get()
+  getAll() {
+    return this.movies;
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: number) {
+    return `This will return one movie ${movieId} `;
+  }
+
+  @Post()
+  create(@Body() movie) {
+    this.movies.push(movie);
+
+    return movie;
+  }
+
+  @Patch(':id')
+  path(@Param('id') movieId: string, @Body() updateData) {
+    this.movies = this.movies.map((movie) => {
+      if (movie.id === +movieId) {
+        return {
+          ...movie,
+          ...updateData,
+        };
+      }
+
+      return movie;
+    });
+
+    return this.movies;
+  }
+
+  //   @Delete(':id')
+  //   remove(@Param('id') movieId: number) {
+  //     console.log('movieId    ', movieId);
+  //     const found = this.movies.find((movie) => movie.id === movieId);
+  //     console.log(found ? 'found' : 'not found');
+
+  //     if (found) {
+  //       this.movies = this.movies.filter((movie) => movie.id !== movieId);
+
+  //       return true;
+  //     }
+
+  //     this.movies = [];
+
+  //     return false;
+  //   }
+
+  //   @Patch(':id')
+  //   patch(@Param('id') movieId: number) {
+  //     return `This will patch a movie ${movieId}`;
+  //   }
+
+  //   @Put('/put')
+  //   put(@Body() movieData) {
+  //     console.log(movieData);
+  //     return movieData;
+  //   }
+}
